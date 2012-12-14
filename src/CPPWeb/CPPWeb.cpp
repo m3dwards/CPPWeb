0a669d099d316f15f241399e9ae18e9178a8e21a
Max Edwards <youwontforgetthis@gmail.com>
2012-12-14 21:32:31 +0000

    First Commit

---
+++ src/CPPWeb/CPPWeb.cpp
@@ -0,0 +1,17 @@
+// CPPWeb.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+//#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}
+
